3ef6660ff4ed62195d0a23e763ba92ca2280c48c
gomesuit <gomesuit@gmail.com>
2016-04-10 15:38:18 +0900

    fluent

--- docker_run.sh
+++ docker_run.sh
@@ -6,4 +6,7 @@ docker run -d \
 	--name nginx \
 	-p 80:80 \
 	--link kindlereport:kindlereport \
+	--log-driver=fluentd \
+	--log-opt=fluentd-address=localhost:24224 \
+	--log-opt=fluentd-tag=docker.{{.FullID}} \
 	kindlereport/nginx
